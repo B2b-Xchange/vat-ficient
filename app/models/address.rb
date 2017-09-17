3800bf6c00091f238af3371bf864e152450c50f0
Marco Hinz <git@b2bxchange.de>
2017-09-17 02:20:06 +0200

    Add more fields to address validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,10 +2,14 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  validates :street_name, presence: true
-  validates :street_number, presence: true
-  validates :post_code, presence: true
-  validates :city, presence: true
+  validates :company, presence: true, if: :company_address?
+  validates :first_name, presence: true, if: :person_address?
+  validates :surname, presence: true, if: :person_address?  
+  validates :street_name, presence: true, length: { maximum: 50 }
+  validates :street_number, presence: true, length: { maximum: 7 }
+  validates :post_code, presence: true, length: { maximum: 8 }
+  validates :city, presence: true, length: { maximum: 50 }
+  
   validate :is_valid_birthday?
 
 
@@ -14,6 +18,14 @@ class Address < ApplicationRecord
     if ((:birthday.is_a?(Date) rescue ArgumentError) == ArgumentError)
       errors.add(:birthday, 'Ungültiges Geburtsdatum angegeben')
     end
+  end
+
+  def company_address?
+    self.kind == 1
+  end
+
+  def person_address?
+    self.kind == 2
   end
   
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,18 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  # 17/09/17
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.your-server.de',
+    port: 587,
+    domain: 'b2bxchange.de',
+    user_name: _,
+    password: _,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 

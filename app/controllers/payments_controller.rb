107bf10827484c52040fcf1fea812026f15dae56
Marco Hinz <git@b2bxchange.de>
2017-09-25 22:52:27 +0200

    set up PayPal config

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -30,7 +30,11 @@ class PaymentsController < ApplicationController
   end
   
   def create
-    # config should be set up
+       
+    PayPal::SDK::REST.set_config(
+      :mode => ENV['PAYPAL_MODE'],
+      :client_id => ENV['PAYPAL_CLIENT_ID'],
+      :client_secret => ENV['PAYPAL_CLIENT_SECRET'])
 
     # build payment object
     order = Order.find(params[:order_id])

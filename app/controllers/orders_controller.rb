c6bc6f686d9564a938a66b8deb66ca8dcab5966c
Marco Hinz <git@b2bxchange.de>
2017-10-03 20:46:43 +0200

    display addresses prettier in Automails

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,76 +26,109 @@ class OrdersController < ApplicationController
     @person_address = @order.addresses.build(address_params[:person_address])
     @person_address.kind = 2
 
-    if order_line_params[:registrations][:registration_germany] == "1"
+    # store order line params to variables for reuse
+    r_de = order_line_params[:registrations][:registration_germany]
+    r_pl = order_line_params[:registrations][:registration_poland]
+    r_cz = order_line_params[:registrations][:registration_czech]
+    r_at = order_line_params[:registrations][:registration_austria]
+    r_es = order_line_params[:registrations][:registration_spain]
+    r_fr = order_line_params[:registrations][:registration_france]
+    r_uk = order_line_params[:registrations][:registration_uk]
+    r_it = order_line_params[:registrations][:registration_italy]
+    r_other = order_line_params[:registrations][:registration_other]
+    
+    if r_de == "1"
       @order.order_lines.build({kind: 1, country_iso_code: "DE"})
     end
 
-    if order_line_params[:registrations][:registration_poland] == "1"
+    if r_pl == "1"
       @order.order_lines.build({kind: 1, country_iso_code: "PL"})
     end
 
-    if order_line_params[:registrations][:registration_czech] == "1"
+    if r_cz == "1"
       @order.order_lines.build({kind: 1, country_iso_code: "CZ"})
     end
 
-    if order_line_params[:registrations][:registration_austria] == "1"
+    if r_at == "1"
       @order.order_lines.build({kind: 1, country_iso_code: "AT"})
     end
 
-    if order_line_params[:registrations][:registration_spain] == "1"
+    if r_es == "1"
       @order.order_lines.build({kind: 1, country_iso_code: "ES"})
     end
 
-    if order_line_params[:registrations][:registration_france] == "1"
+    if r_fr == "1"
       @order.order_lines.build({kind: 1, country_iso_code: "FR"})
     end
 
-    if order_line_params[:registrations][:registration_uk] == "1"
+    if r_uk == "1"
       @order.order_lines.build({kind: 1, country_iso_code: "UK"})
     end
 
-    if order_line_params[:registrations][:registration_italy] == "1"
+    if r_it == "1"
       @order.order_lines.build({kind: 1, country_iso_code: "IT"})
     end
 
-    if order_line_params[:registrations][:registration_other] == "1"
+    if r_other == "1"
       @order.order_lines.build({kind: 1})
     end
 
-    if order_line_params[:declarations][:declaration_germany] == "1"
+    d_de = order_line_params[:declarations][:declaration_germany]
+    d_pl = order_line_params[:declarations][:declaration_poland]
+    d_cz = order_line_params[:declarations][:declaration_czech]
+    d_at = order_line_params[:declarations][:declaration_austria]
+    d_es = order_line_params[:declarations][:declaration_spain]
+    d_fr = order_line_params[:declarations][:declaration_france]
+    d_uk = order_line_params[:declarations][:declaration_uk]
+    d_it = order_line_params[:declarations][:declaration_italy]
+    d_other = order_line_params[:declarations][:declaration_other]
+    
+    if d_de == "1"
       @order.order_lines.build({kind: 2, country_iso_code: "DE"})
     end
 
-    if order_line_params[:declarations][:declaration_poland] == "1"
+    if d_pl == "1"
       @order.order_lines.build({kind: 2, country_iso_code: "PL"})
     end
 
-    if order_line_params[:declarations][:declaration_czech] == "1"
+    if d_cz == "1"
       @order.order_lines.build({kind: 2, country_iso_code: "CZ"})
     end
 
-    if order_line_params[:declarations][:declaration_austria] == "1"
+    if d_at == "1"
       @order.order_lines.build({kind: 2, country_iso_code: "AT"})
     end
 
-    if order_line_params[:declarations][:declaration_spain] == "1"
+    if d_es == "1"
       @order.order_lines.build({kind: 2, country_iso_code: "ES"})
     end
 
-    if order_line_params[:declarations][:declaration_france] == "1"
+    if d_fr == "1"
       @order.order_lines.build({kind: 2, country_iso_code: "FR"})
     end
 
-    if order_line_params[:declarations][:declaration_uk] == "1"
+    if d_uk == "1"
       @order.order_lines.build({kind: 2, country_iso_code: "UK"})
     end
 
-    if order_line_params[:declarations][:declaration_italy] == "1"
+    if d_it == "1"
       @order.order_lines.build({kind: 2, country_iso_code: "IT"})
     end
 
-    if order_line_params[:declarations][:declaration_other] == "1"
+    if d_other == "1"
       @order.order_lines.build({kind: 2})
+    end
+
+    # error message when order positions make no sense
+    combination_error_message = "Registrierung und Deklaration für das gleiche Land ist nicht möglich" 
+    if ((r_de == "1" && d_de == "1") || (r_pl == "1" && d_pl == "1") ||
+        (r_cz == "1" && d_cz == "1") || (r_at == "1" && d_at == "1") ||
+        (r_es == "1" && d_es == "1") || (r_fr == "1" && d_fr == "1") ||
+        (r_fr == "1" && d_fr == "1") || (r_uk == "1" && d_uk == "1") ||
+        (r_it == "1" && d_it == "1"))
+      @order.errors[:base] << combination_error_message
+      render 'new'
+      return
     end
 
     # calculate order value and save to @order
@@ -125,6 +158,15 @@ class OrdersController < ApplicationController
     else
       price += 250
     end
+
+    # error message when no value
+    no_value_error_message = "Es wurde keine Registrierung oder Deklaration ausgewählt"
+    if price < 1
+      @order.errors[:base] << no_value_error_message
+      render 'new'
+      return
+    end
+    
     @order.value = price * 100 # persist in minor units
 
     if !@order.save
